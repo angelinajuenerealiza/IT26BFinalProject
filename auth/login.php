c669092254f499d602fb97ea160471e4461be352
angelinajuenerealiza <angeljuenereal@gmail.com>
2026-05-09 09:53:06 +0800

    login.php

--- auth/login.php
+++ auth/login.php
@@ -14,7 +14,7 @@ if(isset($_POST['login'])) {
 
         $user = mysqli_fetch_assoc($result);
 
-         if(password_verify($password, $user['password'])) {
+        if(password_verify($password, $user['password'])) {
 
             $_SESSION['user_id'] = $user['user_id'];
             $_SESSION['name'] = $user['name'];
